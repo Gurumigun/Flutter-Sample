057a79113ddf2f62a567056da2ef1727ab5307ec
KwonIkYoung <gurumi132@sk.com>
2023-05-14 22:00:50 +0900

    - Chapter 12 : Web RTC

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,68 @@
-import 'package:chapter_6_8/screen/PageViewScreen.dart';
-import 'package:chapter_6_8/screen/webview_screen.dart';
+import 'package:camera/camera.dart';
+import 'package:chapter_6_8/sceen/HomeScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 void main() {
-  runApp(SplashPage());
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
 }
 
-class SplashPage extends StatelessWidget {
-  var logo = Image.asset(
-    'assets/images/logo.png',
-    width: 200,
-    height: 200,
-  );
-  var progress = const CircularProgressIndicator(
-    valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-  );
-
-  SplashPage() : super(key: const Key('SplashPage'));
-
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3), () {
-      Get.offAll(WebViewScreen());
-    });
-
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // home: PageViewStateScreen(),
-      home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-            color: Color(0xFFF99231),
-            ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                logo,
-                progress,
-              ],
-            ),
-          ],
-        ),
-      )),
-    );
-  }
-}
+// late List<CameraDescription> _camera;
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   _camera = await availableCameras();
+//   runApp(app)
+// }
+//
+// class CameraApp extends StatefulWidget {
+//   const CameraApp({Key? key}) : super(key: key);
+//
+//   @override
+//   State<CameraApp> createState() => _CameraAppState();
+// }
+//
+// class _CameraAppState extends State<CameraApp> {
+//   late CameraController controller;
+//
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//
+//   }
+//
+//   initCamera() async {
+//     try {
+//       controller = CameraController(_camera[0], ResolutionPreset.max);
+//       await controller.initialize();
+//       setState(() {});
+//     } catch( e) {
+//       if (e is CameraException) {
+//         print(e.description);
+//       }
+//     }
+//   }
+//
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     if (!controller.value.isInitialized) {
+//       return Container();
+//     }
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Camera App'),
+//         ),
+//         body: CameraPreview(controller),
+//       ),
+//     );
+//   }
+// }
